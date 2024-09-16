3b1e74ae68b0828cb0370801c11cc7d2147bb79f
Steve@V4E <steve@voice4equity.com>
2024-09-16 12:27:02 -0700

    Updated requirements.txt

--- run.py
+++ run.py
@@ -4,14 +4,10 @@ from app.models import User, Type, UserType, UserProfile, Category, Activity, Ac
 from dotenv import load_dotenv
 import os
 
-print("Loading environment variables")
-load_dotenv()  # Load environment variables from .env file
-
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    print("Starting Flask server")
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, port=port)
